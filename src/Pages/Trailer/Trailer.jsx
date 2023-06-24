e740c997852a2a153eb1ae5e9a32d94d5018191f
Shincy <shincyshnz@gmail.com>
2023-06-25 00:33:19 +0530

    added trailer page

--- src/Pages/Trailer/Trailer.jsx
+++ src/Pages/Trailer/Trailer.jsx
@@ -3,13 +3,14 @@ import { useParams } from "react-router-dom";
 import { useMovies } from "../../CustomHooks/useMovies";
 import { TMDB_IMAGE_URL, NO_IMAGE_URL } from "../../constants/TMDB_API";
 import YouTube from "react-youtube";
+import "./trailer.css";
 
 const Trailer = () => {
   const { movieTrailer, getMovieTrailer } = useMovies();
   const params = useParams();
   const opts = {
-    width: "1200",
-    height: "700",
+    width: "100%",
+    height: "100%",
   };
 
   useEffect(() => {
@@ -24,24 +25,31 @@ const Trailer = () => {
     const trailer = movieTrailer.data.videos.results.find(
       (movie) => movie.name === "Official Trailer"
     );
-    return <YouTube videoId={trailer?.key} opts={opts} onReady={onReady} />;
+    return (
+      <YouTube
+        className="youtube-container"
+        videoId={trailer?.key}
+        opts={opts}
+        onReady={onReady}
+      />
+    );
   };
 
   return (
     <>
-      <div className="trailer-container mx-auto w-100 aspect-video">
+      <div className="trailer-container mx-auto w-100">
         {movieTrailer && renderTrailer()}
       </div>
-      <div className="flex justify-center gap-5 flex-wrap mt-8">
+      <div className="w-100 flex justify-center gap-5 flex-wrap mt-8">
         {movieTrailer &&
           movieTrailer.data.credits?.cast.map((actor) => {
             return (
               <div
-                className="mx-10 w-60 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700"
+                className="cast-container mx-10 bg-white border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-700"
                 key={actor.id}
               >
                 <img
-                  className="rounded-t object-contain w-60"
+                  className="cast-image rounded-t w-60"
                   src={
                     actor.profile_path
                       ? `${TMDB_IMAGE_URL}${actor.profile_path}`
@@ -49,8 +57,8 @@ const Trailer = () => {
                   }
                   alt="actor-profile"
                 ></img>
-                <div className="p-5">
-                  <h5 className="w-100 text-gray-900 font-bold text-2xl tracking-tight mb-2 dark:text-white">
+                <div className="cast-text p-5">
+                  <h5 className="text-gray-900 w-100 font-bold text-2xl tracking-tight mb-2 dark:text-white">
                     {actor.original_name}
                   </h5>
                   <p className="w-100 font-normal text-gray-700 mb-3 dark:text-gray-400">
